49cfeaed6826738dd6aaf738b46b8cd994c8c837
小镇靓仔 <silentflutehustle@gmail.com>
2021-09-19 10:32:01 +0800

    Update webpack.skpm.config.js

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -28,7 +28,7 @@ module.exports = (config) => {
       ...config.resolve,
       alias: {
         ...config.resolve.alias,
-        'react-sketchapp': path.resolve(__dirname, './Svg.js'),
+        'react-native-svg': path.resolve(__dirname, './Svg.js'),
       },
     };
   }
