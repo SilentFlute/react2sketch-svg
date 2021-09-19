b63b80066bb86ebda1795aec33a54c937b9d7e2b
小镇靓仔 <silentflutehustle@gmail.com>
2021-09-19 16:43:40 +0800

    Update webpack.skpm.config.js

--- webpack.skpm.config.js
+++ webpack.skpm.config.js
@@ -1,35 +1,33 @@
 const path = require('path');
 
 module.exports = (config) => {
-  if (process.env.LOCAL_DEV) {
-    config.module.rules[1].test = /^(?!.*\.(jsx?|tsx?|json|svg|md|nib|xib|framework|xcodeproj|xcworkspace|xcworkspacedata|pbxproj)$).*/;
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@skpm/babel-preset'],
-          },
+  config.module.rules[1].test = /^(?!.*\.(jsx?|tsx?|json|svg|md|nib|xib|framework|xcodeproj|xcworkspace|xcworkspacedata|pbxproj)$).*/;
+  config.module.rules.push({
+    test: /\.svg$/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@skpm/babel-preset'],
         },
-        {
-          loader: '@svgr/webpack',
-          options: {
-            native: true,
-            svgoConfig: {
-              plugins: [{ convertPathData: false }],
-            },
-            babel: false,
-          },
-        },
-      ],
-    });  
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        'react-native-svg': path.resolve(__dirname, './Svg.js'),
       },
-    };
-  }
+      {
+        loader: '@svgr/webpack',
+        options: {
+          native: true,
+          svgoConfig: {
+            plugins: [{ convertPathData: false }],
+          },
+          babel: false,
+        },
+      },
+    ],
+  });  
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.resolve.alias,
+      'react-native-svg': path.resolve(__dirname, './Svg.js'),
+    },
+  };
 };
